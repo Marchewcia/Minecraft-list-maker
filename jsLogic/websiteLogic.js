a0ae624df0d706f7ed0e365dbc26e4201f20eac3
Marchewcia <michalgolasinski6@gmail.com>
2024-12-05 00:23:37 +0100

    added delete without changing number and del function because dunno how and it's 00:20

--- jsLogic/websiteLogic.js
+++ jsLogic/websiteLogic.js
@@ -7,5 +7,47 @@ function loadOrNew(){
 
 function addNewColumn(){
     count++;
-    main.innerHTML += '<div class="row" id="row' + count + '"><div class="num">No.</div><div class="mname">Block/Item name</div><div class="amount">Total amount(Stacks, Blocks)</div><div class="del">Delete item</div></div></div>';
+    //rewrite with document.createElement();
+    main.innerHTML += '<div class="row" id="row' + count + '"><div class="num">' + count + '</div><div class="mname">Block/Item name</div><div class="amount">Total amount(Stacks, Blocks)</div><div class="del"><button class="buttonDelete" onclick="deleteColumn(row' + count + ')">Delete element</button></div></div></div>';
+}
+
+function deleteColumn(a){
+    a.remove();
+    othertabs = true;
+    tabindex = 1;
+    tabiterator = 1;
+    while(othertabs){
+        rowcurrent = document.getElementById("row" + tabindex);
+        //reset count if no rows present
+        if(rowcurrent == null && document.getElementById("row" + (tabindex + 1)) == null){
+            count = 0;
+            othertabs = false;
+            return;
+        }
+        //check if there are any other tabs
+        else if(tabindex > count){
+            othertabs = false;
+            count = tabiterator;
+            return;
+        }
+        //skip non existent rows
+        else if(rowcurrent == null){
+            tabindex++;
+        }
+        else{
+
+            //dunno that stupid setAtribute ain't working
+            //to fix
+            rowcurrent.id = "row" + tabiterator;
+            rowcurrent.getElementsByClassName("num").innerHTML = tabiterator;
+            delColumn = rowcurrent.getElementsByClassName("del");
+            delColumn = delColumn.getElementsByClassName("buttonDelete");
+            delColumn.setAttribute("onclick", "deleteColumn(row" + tabiterator + ")");
+            // rowcurrent.getElementsByClassName("del").setAttribute("onclick", "deleteColumn(row" + tabiterator + ")");
+            // newDel = document.createElement('<button class="buttonDelete" onclick="deleteColumn(row' + tabiterator + ')">Delete element</button>');
+            // rowcurrent.replaceChild(newDel, oldDel);
+            tabindex++;
+            tabiterator++;
+        }
+    }
 }
