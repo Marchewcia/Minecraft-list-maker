179859877115190b90f1bfb065565f1e7bb99f61
Marchewcia <michalgolasinski6@gmail.com>
2024-12-08 00:01:05 +0100

    fixed and cleaned delete, made variables accesible only in their scope

--- jsLogic/websiteLogic.js
+++ jsLogic/websiteLogic.js
@@ -1,29 +1,33 @@
+let count = 2;
+
 function loadOrNew(){
     input = true; //temp
     if(input){//true:new
-        count = 2;
         initializeFirstMenu();
     }
 }
 
 function initializeFirstMenu(){
-    firstMenu = document.createElement("select");
-    blockname = ["dirt", "stone", "cobblestone", "oak_log", "oak_planks"];
+    let firstMenu = document.createElement("select");
+    let blockname = ["dirt", "stone", "cobblestone", "oak_log", "oak_planks"];
+
     for(i = 0; i < blockname.length; i++){
         firstMenu.appendChild(document.createElement("option"));
         firstMenu.getElementsByTagName("option")[i].setAttribute("value", blockname[i]);
         firstMenu.getElementsByTagName("option")[i].innerHTML = blockname[i];
+
         if(i == 0){
             firstMenu.getElementsByTagName("option")[i].setAttribute("selected", "");
         }
     }
+    
     document.getElementsByClassName("mname")[1].appendChild(firstMenu);
 }
 
 function addNewColumn(){
-    oldRow = document.getElementById("row1");
-    newRow = oldRow.cloneNode(true);
-    currentRow = "row" + count;
+    let oldRow = document.getElementById("row1");
+    let newRow = oldRow.cloneNode(true);
+    let currentRow = "row" + count;
 
     newRow.setAttribute("id", currentRow);
 
@@ -89,17 +93,17 @@ function setStackAmount(columnNumber){
     }
 }
 
-function deleteColumn(a){
-    if(a == row1 && count == 2){
+function deleteColumn(columnNumber){
+    if(columnNumber == row1 && count == 2){
         alert("You cannot delete last element from the list");
     }
     else{
-        a.remove();
-        othertabs = true;
-        tabindex = 1;
-        tabiterator = 1;
+        columnNumber.remove();
+        let othertabs = true;
+        let tabindex = 1;
+        let tabiterator = 1;
         while(othertabs){
-            rowcurrent = document.getElementById("row" + tabindex);
+            let rowcurrent = document.getElementById("row" + tabindex);
             //check if there are any other tabs
             if(tabindex > count){
                 othertabs = false;
@@ -112,14 +116,15 @@ function deleteColumn(a){
             }
             else{
                 rowcurrent.id = "row" + tabiterator;
-                numberChange = rowcurrent.getElementsByClassName("num");
-                numberChange = numberChange[0];
-                numberChange.innerHTML = tabiterator;
-                delColumn = rowcurrent.getElementsByClassName("del");
-                delColumn = delColumn[0];
-                delColumn = delColumn.getElementsByClassName("buttonDelete");
-                delColumn = delColumn[0];
-                delColumn.setAttribute("onclick", "deleteColumn(row" + tabiterator + ")");
+                
+                numberChange = rowcurrent.getElementsByClassName("num")[0].innerHTML = tabiterator;
+
+                rowcurrent.getElementsByClassName("amount")[0].setAttribute("id", "idBlocks" + tabiterator);
+                rowcurrent.getElementsByTagName("button")[0].setAttribute("onclick", "setTotalAmount(" + tabiterator + ")");
+                rowcurrent.getElementsByTagName("button")[1].setAttribute("onclick", "setStackAmount(" + tabiterator + ")");
+
+                rowcurrent.getElementsByClassName("buttonDelete")[0].setAttribute("onclick", "deleteColumn(row" + tabiterator + ")");
+                
                 tabindex++;
                 tabiterator++;
             }
