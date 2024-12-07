73cc763123685fb552dc6db3c45d1a49ad6178a2
Marchewcia <michalgolasinski6@gmail.com>
2024-12-07 23:43:52 +0100

    created amount in stacks and items, also amount in total

--- jsLogic/websiteLogic.js
+++ jsLogic/websiteLogic.js
@@ -25,13 +25,68 @@ function addNewColumn(){
     newRow = oldRow.cloneNode(true);
     currentRow = "row" + count;
 
-    newRow.setAttribute("id", currentRow)
-    newRow.getElementsByClassName("num")[0].innerHTML = count
-    newRow.getElementsByClassName("buttonDelete")[0].removeAttribute("onclick");
-    newRow.getElementsByClassName("buttonDelete")[0].setAttribute("onclick", "deleteColumn(row" + count + ")");
+    newRow.setAttribute("id", currentRow);
+
+    newRow.getElementsByClassName("num")[0].innerHTML = count;
     
+    newRow.getElementsByTagName("div")[2].setAttribute("id", "idBlocks" + count);
+    newRow.getElementsByTagName("button")[0].setAttribute("onclick", "setTotalAmount(" + count + ")");
+    newRow.getElementsByTagName("button")[0].innerHTML = "0 Total Items";
+
+    newRow.getElementsByTagName("button")[1].setAttribute("onclick", "setStackAmount(" + count + ")");
+    newRow.getElementsByTagName("button")[1].innerHTML = "0 Stacks 0 Items";
+    
+    newRow.getElementsByClassName("buttonDelete")[0].setAttribute("onclick", "deleteColumn(row" + count + ")");
+
     main.appendChild(newRow);
     count++;
+}
+
+function setTotalAmount(columnNumber){
+    let input = prompt("Set total amount of item or a block");
+    if(input != null){
+        input = parseInt(input);
+
+        if(Number.isInteger(input)){
+            let parent = document.getElementById("idBlocks" + columnNumber);
+            let stacks = 0;
+    
+            for(let total = parseInt(input); total >= 64; total -= 64){
+                stacks++;
+            }
+
+            let items = input - (stacks * 64);
+
+            parent.getElementsByTagName("button")[0].innerHTML = input + " Total Items";
+            parent.getElementsByTagName("button")[1].innerHTML = stacks + " Stacks " + items + " Items";
+        }
+        else{
+            alert("Invalid input, input was not a number");
+        }
+    }
+}
+
+function setStackAmount(columnNumber){
+    let inputStacks = prompt("Set amount of Stacks of item or a block");
+    let inputItems = null;
+    if(inputStacks != null){
+        inputItems = prompt("Set rest (that didn't add to a full stack) of item or a block");
+    }
+    if(inputItems != null){
+        inputStacks = parseInt(inputStacks);
+        inputItems = parseInt(inputItems);
+
+        if(Number.isInteger(inputStacks) && Number.isInteger(inputItems)){
+            let parent  = document.getElementById("idBlocks" + columnNumber);
+            let inputTotal = (parseInt(inputStacks) * 64) + parseInt(inputItems);
+    
+            parent.getElementsByTagName("button")[0].innerHTML = inputTotal + " Total Items";
+            parent.getElementsByTagName("button")[1].innerHTML = inputStacks + " Stacks " + inputItems + " Items";    
+        }
+        else{
+            alert("Invalid input, input was not a number");
+        }
+    }
 }
 
 function deleteColumn(a){
